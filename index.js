d0bca859105c5cbea417ce14d59a02ae66a3a28f
Руднев Игорь Валерьевич <rudnev_iv@its.local>
2024-06-17 11:57:36 +0700

    some changes

--- index.js
+++ index.js
@@ -5,20 +5,34 @@ const { Bot, Keyboard, InlineKeyboard, GrammyError,HttpError  } = require('gramm
 const bot = new Bot(process.env.BOT_API_KEY)
 
 bot.command('start', async (ctx) => {
-    const startKeyboard = new Keyboard()
-    .text('Вопрос 1')
-    .text('Вопрос 2')
-    .row()
-    .text('Вопрос 3')
-    .text('Вопрос 4')
-    .resized()
-    await ctx.reply('Привет! \nВот и пришло время квеста')
-    await ctx.reply('С чего начнем? Выбери тему вопроса 👇', {
-        reply_markup: startKeyboard
-    })
+    const startKeyboard = new Keyboard() 
+    await ctx.reply('Привет! \nВот и пришло время приключений!')
+    await ctx.reply('👇Скорей нажимай меню и начинай квест')
 })
 
-bot.hears(["Вопрос 1", "Вопрос 2", "Вопрос 3", "Вопрос 4"], async (ctx) => {
+bot.command('about', async (ctx) => {
+  await ctx.reply('Новое приключение о компании в рамках корпоратива')
+})
+
+bot.command('quest', async (ctx) => {
+  const questKeyboard = new Keyboard()
+  .text('Получить вопрос')
+  .text('Статус заданий')
+  .row()
+  .text('Баланс')
+  .text('Обратиться в поддержку', 'support')
+  .resized()
+  await ctx.reply('Скорей начинай наш квест, чтобы заработать баллы', {
+    reply_markup: questKeyboard
+})
+})
+const backKeyboard = new InlineKeyboard().text('< Назад в меню', 'back');
+bot.hears('Обратиться в поддержку', async (ctx) => {
+  await ctx.reply('Напишите Ваш вопрос', { reply_markup: backKeyboard,
+  });
+})
+
+bot.hears(["Получить вопрос"], async (ctx) => {
     const inlineKeyboard = new InlineKeyboard()
     .text("Получить ответ", JSON.stringify({
         type: ctx.message.text,
@@ -29,6 +43,12 @@ bot.hears(["Вопрос 1", "Вопрос 2", "Вопрос 3", "Вопрос 4
         reply_markup: inlineKeyboard
     })
 })
+
+bot.api.setMyCommands([
+  {command: 'start', description: 'Запуск бота'}, 
+  {command: 'about', description: 'Что тут происходит?'}, 
+  {command: 'quest', description: 'Квест'} 
+])
 
 bot.on('callback_query:data', async (ctx) => {
     if (ctx.callbackQuery.data === 'cancel') {
@@ -41,16 +61,21 @@ bot.on('callback_query:data', async (ctx) => {
     await ctx.answerCallbackQuery()
 })
 
+
+
+
+
+
 bot.catch((err) => {
     const ctx = err.ctx;
     console.error(`Ошибка обработки ${ctx.update.update_id}:`);
     const e = err.error;
     if (e instanceof GrammyError) {
-      console.error("Ошибка запроса:", e.description);
+      console.error("Ошибка запроса: ", e.description);
     } else if (e instanceof HttpError) {
-      console.error("Кажется проблема на стороне Телеграма:", e);
+      console.error("Кажется проблема на стороне Телеграма: ", e);
     } else {
-      console.error("Неизвестная ошибка:", e);
+      console.error("Неизвестная ошибка: ", e);
     } 
   });
 
